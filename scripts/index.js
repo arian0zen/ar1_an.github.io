bb8db51252c5baa58f954e576668ff8062409520
Arian Shaikh <ariansk90@gmail.com>
2022-06-18 18:14:05 +0530

    18th june day 3

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,13 @@ $(document).ready(function () {
   setTimeout(function () {
     $(".moon").addClass("is-moon-in-the-sky");
   }, 1150);
+  setTimeout(function () {
+    $(".blockchain").addClass("is-in-view");
+  }, 550);
+  setTimeout(function () {
+    $(".b_chain").addClass("is-in-view");
+  }, 1150);
+
 });
 
 $('.circle').on('click', function () {
@@ -14,4 +21,10 @@ $('.circle').on('click', function () {
   var element = document.body;
   element.classList.toggle("dark-mode");
   $(".moon").toggleClass("moon_is_on");
+});
+
+$('.blockchain').on('click', function () {
+  // console.log("clicked");
+  var element = document.body;
+  element.classList.toggle("chain_mode");
 });
